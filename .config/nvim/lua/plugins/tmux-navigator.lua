a8056a0341e8d66b380a2eaecfefbb507c89afe0
Thomas Bouasli <=>
2024-05-19 21:27:29 -0300

    feat: added tmux config

---
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd> TmuxNavigateLeft<cr>", "Window Left" },
+      { "<C-l>", "<cmd> TmuxNavigateRight<cr>", "Window Left" },
+      { "<C-j>", "<cmd> TmuxNavigateDown<cr>", "Window Left" },
+      { "<C-k>", "<cmd> TmuxNavigateUp<cr>", "Window Left" },
+    },
+  },
+}
